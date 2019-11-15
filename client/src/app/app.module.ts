e6a5277dbaca7442aa408602f78ab9c3c612246b
dixy404 <dixy@blic.net>
2019-11-15 05:16:32 -0800

    ..

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material/app-material.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-
+import { JwtInterceptor } from './login-helpers/jwt.interceptor';
+import { ErrorInterceptor } from './login-helpers/error.interceptor';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-      
+       
   
   ],
   imports: [
@@ -25,7 +26,10 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
    
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
