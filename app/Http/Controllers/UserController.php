c3f3ecd3cdd087b603242265ccb889409fbe87bd
Igor <tehnolog.banjaluka@gmail.com>
2019-11-19 10:44:48 +0100

    nove validacije

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,6 +33,7 @@
              'lastName' => 'required|string|max:255',
              'dateOfBirth' => 'required|string|max:255',
              'address' => 'required|string|max:255',
+             'ssn' => 'string|max:255|unique:users',
              'email' => 'required|string|email|max:255|unique:users',
              'password' => 'required|string|min:8|confirmed',
          ]);
