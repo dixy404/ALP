09dd6762eef1c85d4fef5091f3cb266174b4ee4d
Igor <tehnolog.banjaluka@gmail.com>
2019-11-19 13:02:49 +0100

    novi model za planinarsko društvo

---
+++ app/Club.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\Club as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Club extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+        /**
+         * The attributes that are mass assignable.
+         *
+         * @var array
+         */
+        protected $fillable = [
+            'clubName', 'clubPresident', 'clubSecretary', 'foundedIn', 'vision', 'mission', 'address', 'email', 'password'];
+
+        /**
+         * The attributes that should be hidden for arrays.
+         *
+         * @var array
+         */
+        protected $hidden = [
+            'password',  'remember_token',
+        ];
+
+        public function getJWTIdentifier()
+        {
+            return $this->getKey();
+        }
+        public function getJWTCustomClaims()
+        {
+            return [];
+        }
+}
