c89e0d313dcfdd173c5f7eed74204a57c640b11c
dixy404 <dixy@blic.net>
2019-11-19 12:31:40 +0100

    registracija kluba

---
+++ client/src/app/model/club.model.ts
@@ -0,0 +1,13 @@
+export class Club {
+    clubName: string;
+    clubPresident: string;
+    clubSecretary: string;
+    foundedIn: Date;
+    vision: string;
+    mission: string;
+    address: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+
+}
--- client/src/app/registration/registration-club/registration-club.component.ts
+++ client/src/app/registration/registration-club/registration-club.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Club } from 'src/app/model/club.model';
+import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
   selector: 'app-registration-club',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationClubComponent implements OnInit {
 
-  constructor() { }
+  public form: FormGroup; 
+  public club: Club = new Club();
+
+
+  constructor(public formBuilder: FormBuilder,
+    private registrationService: RegistrationService
+    ) { }
 
   ngOnInit() {
-  }
+
+    this.form = this.formBuilder.group({
+      clubName: [this.club.clubName, Validators.required],
+      clubPresident: [this.club.clubPresident, Validators.required],
+      clubSecretary: [this.club.clubSecretary, Validators.required],
+      foundedIn: [this.club.foundedIn, Validators.required],
+      vision: [this.club.vision, Validators.required],
+      mission: [this.club.mission, Validators.required],
+      address: [this.club.address, Validators.required], 
+      email: [this.club.email, Validators.email],
+      password: [this.club.password, Validators.required],
+      password_confirmation : [this.club.password_confirmation, Validators.required],
+      
+    });
+}
+
+save({value, valid}: {value: Club, valid: boolean}) { 
+  console.log(this.form.value)
+  const {clubName, clubPresident, clubSecretary, foundedIn, vision, mission, address, email, password, password_confirmation } = this.form.value
+
+  
+    this.registrationService.registerClub(clubName, clubPresident, clubSecretary, foundedIn, vision, mission, address, email, password, password_confirmation)
+    .subscribe(data => console.log(data))
+ 
+ 
+}
 
 }
