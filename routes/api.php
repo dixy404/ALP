42170e25320a3d3717c56bf40c26d302325b51dc
Igor <tehnolog.banjaluka@gmail.com>
2019-11-22 15:52:40 +0100

    nove rute za event

--- routes/api.php
+++ routes/api.php
@@ -21,23 +21,28 @@ use Illuminate\Http\Request;
 
 Route::post('registeruser', 'UserController@register');
 Route::post('registerclub', 'ClubController@register');
-    Route::post('loginuser', 'UserController@authenticate');
-    Route::post('loginclub', 'ClubController@authenticate');
-    Route::get('open', 'DataController@open');
+Route::post('loginuser', 'UserController@authenticate');
+Route::post('loginclub', 'ClubController@authenticate');
+Route::get('index', 'ClubController@index');
+Route::get('index', 'EventController@index');
+Route::get('open', 'DataController@open');
+Route::post('createevent', 'EventController@register');
+
+Route::get('closed', 'DataController@closed');
+Route::get('/showuser/{id}', 'UserController@show');
+Route::get('/edituser/{id}', 'UserController@edit');
+Route::put('/updateuser/{id}', 'UserController@update');
+Route::delete('/deleteuser/{id}', 'UserController@destroy');
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::get('user', 'UserController@getAuthenticatedUser');
+Route::group(['middleware' => ['role:moderator']], function () {
     
-        
-        Route::get('closed', 'DataController@closed');
-        Route::get('/showuser/{id}', 'UserController@show');
-        Route::get('/edituser/{id}', 'UserController@edit');
-        Route::put('/updateuser/{id}', 'UserController@update');
-        Route::delete('/deleteuser/{id}', 'UserController@destroy');
-    Route::group(['middleware' => ['jwt.verify']], function() {
-        Route::get('user', 'UserController@getAuthenticatedUser');
-        //Route::get('user', 'UserController@index');
-        
-        //Route::get('/showuser/{id}', 'RegisterMemeberController@show');
-        //Route::get('/members', 'RegisterMemeberController@index');
-        });
+    });   
+    //Route::get('user', 'UserController@index');
+
+    //Route::get('/showuser/{id}', 'RegisterMemeberController@show');
+    //Route::get('/members', 'RegisterMemeberController@index');
+});
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
     //return $request->user();
 //});
