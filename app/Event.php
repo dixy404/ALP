a5404539b40ac57627b5820407313c01aeb15e94
Igor <tehnolog.banjaluka@gmail.com>
2019-11-22 02:35:27 +0100

    kreiran model Event

---
+++ app/Event.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Event extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['tripDate', 'tripTime', 'tripName', 'location', 'description', 'tripDuration', 'departureTime', 'accommodation', 'season', 'difficultyLevel', 'equipment', 'elevation', 'hours', 'trailLength', 'trailType', 'terrainType'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+          'remember_token',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
