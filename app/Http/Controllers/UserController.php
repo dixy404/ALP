75e32c6e57511d9d23faaa6b9f92d789170ce89d
Igor <tehnolog.banjaluka@gmail.com>
2019-11-25 14:13:13 +0100

    ...

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -68,7 +68,26 @@
         }
         public function show($id){ 
           header("Access-Control-Allow-Origin: *");
-         $user = User::find($id);
+          try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                    return response()->json(['user_not_found'], 404);
+            }
+
+    } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+    } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+    } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+    }
+          $user = User::find($id);
          return response()->json(compact('user'),201);
 
 
