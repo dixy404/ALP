abd1219ec403840f800379289110d22f10240312
dixy404 <dixy@blic.net>
2019-12-05 15:08:48 +0100

    ....

--- client/src/app/services/registration.service.ts
+++ client/src/app/services/registration.service.ts
@@ -10,7 +10,7 @@ export class RegistrationService {
   constructor(private http: HttpClient) { }
 
   register(name, address, email, phoneNumber, lastName, dateOfBirth, placeOfBirth, passportId, idNumber, ssn, nationality, occupation, bloodType, password, password_confirmation, thumbnail,) {
-    return this.http.post('http://localhost:8000/api/registeruser', { name: name, address: address, email: email, phoneNumber: phoneNumber, lastName: lastName, dateOfBirth: dateOfBirth, placeOfBirth: placeOfBirth, passportId: passportId, idNumber: idNumber, ssn: ssn, nationality: nationality, occupation: occupation, bloodType: bloodType, password: password, password_confirmation: password_confirmation, thumbnail: thumbnail })
+    return this.http.post('http://localhost:8000/api/registeruser', { name: name, address: address, email: email, phoneNumber: phoneNumber, lastName: lastName, dateOfBirth: dateOfBirth, placeOfBirth: placeOfBirth, passportId: passportId, idNumber: idNumber, ssn: ssn, nationality: nationality, occupation: occupation, bloodType: bloodType, password: password, password_confirmation: password_confirmation, thumbnail: thumbnail }, { withCredentials: true })
   }
 
  
