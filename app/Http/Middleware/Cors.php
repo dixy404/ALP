c132ec50a4380e1694256a7818d2a809df0614ac
Igor <tehnolog.banjaluka@gmail.com>
2019-12-06 09:23:11 +0100

    prepravljen cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,23 +7,11 @@ class Cors
 {
   public function handle($request, Closure $next)
   {
-    return [
-        /*
-        |--------------------------------------------------------------------------
-        | Laravel CORS
-        |--------------------------------------------------------------------------
-        |
-        | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
-        | to accept any value.
-        |
-        */
-       'supportsCredentials' => false,
-       'allowedOrigins' => ['*'],
-       'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
-       'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
-       'exposedHeaders' => [],
-       'maxAge' => 0,
-   ];
+      header('Access-Control-Allow-Credentials', false);
+      header('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
+      header('Access-Control-Allow-Methods', '*');
+      header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Authorization, Content-Type, Accept');
+      return $next($request);    
 }
 
 }
