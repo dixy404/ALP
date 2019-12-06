96d2ecf1153682808d3df88a9e1ed53f0b39d62d
Igor <tehnolog.banjaluka@gmail.com>
2019-12-06 09:23:57 +0100

    dodana adresa sa frontenda u origins

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     */
    
     'supportsCredentials' => false,
-    'allowedOrigins' => ['*'],
+    'allowedOrigins' => ['http://127.0.0.1:4200' ],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'],
